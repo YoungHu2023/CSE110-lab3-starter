c4589fee36f873b1b42184d42550a2e092cb18b2
Yang Hu <huyang2022@126.com>
2026-01-20 18:28:18 -0800

    update

--- src/test/java/edu/ucsd/spendingtracker/repository/ExpenseRepositoryTest.java
+++ src/test/java/edu/ucsd/spendingtracker/repository/ExpenseRepositoryTest.java
@@ -1,14 +1,14 @@
 package edu.ucsd.spendingtracker.repository;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import edu.ucsd.spendingtracker.datasource.InMemoryDataSource;
 import edu.ucsd.spendingtracker.model.Category;
 import edu.ucsd.spendingtracker.model.Expense;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ExpenseRepositoryTest {
@@ -36,14 +36,13 @@ public class ExpenseRepositoryTest {
         double total = repository.getTotal();
 
 
-        assertEquals(40.50, total);
+        assertEquals(30.50, total);
     }
 
 
     @Test
     @DisplayName("Test that getTotal() returns 0.0 when no expenses exist")
     void testGetTotalWithEmptyList() {
-        assertEquals(1.0, repository.getTotal()+2);
+        assertEquals(0.0, repository.getTotal());
     }
 }
-
